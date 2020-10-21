2ff4ec34b26f3f8cdcf0a54dc8309aa825f797be
Edyta Bednarz <edy.b.bednarz@gmail.com>
2020-10-21 21:04:35 +0100

    Added Buzz logic and FizzBuzz logic

--- fizzBuzz/fizz_buzz.py
+++ fizzBuzz/fizz_buzz.py
@@ -1,7 +1,13 @@
 def FizzBuzz(num):
-    if num % 3 == 0:
+    if num % 15 == 0:
+        print('FizzBuzz')
+        return 'FizzBuzz'
+    elif num % 3 == 0:
         print('Fizz')
         return 'Fizz'
+    elif num % 5 == 0:
+        print('Buzz')
+        return 'Buzz'
     else:
         print(num) 
         return num
--- fizzBuzz/fizzbuzz_test.py
+++ fizzBuzz/fizzbuzz_test.py
@@ -13,8 +13,14 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEquals(FizzBuzz(6), 'Fizz')
         self.assertEquals(FizzBuzz(33), 'Fizz')  
    
-
-     
+    def test_returns_Buzz_when_number_is_devisable_by_5(self):
+        self.assertEquals(FizzBuzz(5), 'Buzz') 
+        self.assertEquals(FizzBuzz(10), 'Buzz')
+        self.assertEquals(FizzBuzz(55), 'Buzz')
+       
+    def test_returns_FizzBuzz_when_number_is_devisable_by_5_and_3(self):
+        self.assertEquals(FizzBuzz(15), 'FizzBuzz') 
+        self.assertEquals(FizzBuzz(105), 'FizzBuzz')
 
 
 if __name__ == '__main__':
